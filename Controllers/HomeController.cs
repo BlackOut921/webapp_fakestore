b73803d46ce876a7371cc17eaf8d6d96889a6ade
BlackOut 921 <conorhowe921@gmail.com>
2024-09-13 17:55:35 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_fakestore.Controllers
+{
+	[AllowAnonymous]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index() => View();
+	}
+}
