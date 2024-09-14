3bc43c637aed2397c84f63a544deacc986dd14ad
BlackOut 921 <conorhowe921@gmail.com>
2024-09-14 14:47:18 +0100

    Downscaled images

--- wwwroot/JavaScript.js
+++ wwwroot/JavaScript.js
@@ -1,1 +1,34 @@
 ﻿"use strict";
+
+async function GetProducts() {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (response.ok) {
+            const data = await response.json();
+
+            //Clothing
+            const mens = data.filter(i => i.category === "men's clothing");
+            console.log(mens);
+            const womens = data.filter(i => i.category === "women's clothing");
+            console.log(womens);
+
+            //Jewelery
+            const jewelery = data.filter(i => i.category === "jewelery");
+            console.log(jewelery);
+
+            //Electronics
+            const electronics = data.filter(i => i.category === "electronics");
+            console.log(electronics);
+        }
+        else {
+            throw new Error("Request error");
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+window.onload = () => {
+    GetProducts();
+};
